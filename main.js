6071ea756ac6b387818bc5a1b329edbcf3f56e26
Jonathan Pinto Sperafico <jonathanpspeafico@outlook.com>
2024-06-03 11:26:54 +0300

    mobile landscape

---
+++ main.js
@@ -0,0 +1,16 @@
+const urls = [
+    "https://jsperafico.me",
+    "https://www.linkedin.com/in/jonathanpintosperafico/",
+    "https://github.com/jsperafico",
+    "mailto:jonathanpsperafico@hotmail.com"
+];
+
+window.onload = function(e) {
+    var socials = document.querySelectorAll("#business-card .back aside ul li");
+    for (let index = 0; index < socials.length; index++) {
+        const element = socials[index];
+        element.addEventListener("click", function(e) {
+            window.open(urls[index], '_blank').focus();
+        });
+    }
+}
