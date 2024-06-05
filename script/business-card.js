89b47ac5c7419018ce9b4f3e575afff020a4c496
Jonathan Pinto Sperafico <jonathanpspeafico@outlook.com>
2024-06-05 13:20:07 +0300

    transition working

--- script/business-card.js
+++ script/business-card.js
@@ -5,12 +5,32 @@ const urls = [
     "mailto:jonathanpsperafico@hotmail.com"
 ];
 
+function openCurriculum() {
+    window.open("curriculum.html", '_self').focus();
+}
+
 window.onload = function(e) {
     var socials = document.querySelectorAll("#business-card .back aside ul li");
     for (let index = 0; index < socials.length; index++) {
         const element = socials[index];
         element.addEventListener("click", function(e) {
+            e.preventDefault();
             window.open(urls[index], '_blank').focus();
         });
     }
+
+    var article = document.querySelector("article");
+    article.addEventListener('dblclick', () => openCurriculum());
+
+    // Handle double-tap
+    let lastTapTime = 0;
+    article.addEventListener('touchstart', (e) => {
+        const currentTime = new Date().getTime();
+        const tapInterval = currentTime - lastTapTime;
+        if (tapInterval < 300 && tapInterval > 0) {
+            e.preventDefault();
+            openCurriculum();
+        }
+        lastTapTime = currentTime;
+    });
 }
--- script/curriculum.js
+++ script/curriculum.js
@@ -5,12 +5,32 @@ const urls = [
     "mailto:jonathanpsperafico@hotmail.com"
 ];
 
+function openBusinessCard() {
+    window.open("index.html", '_self').focus();
+}
+
 window.onload = function(e) {
     var socials = document.querySelectorAll("ul.social li");
     for (let index = 0; index < socials.length; index++) {
         const element = socials[index];
         element.addEventListener("click", function(e) {
+            e.preventDefault();
             window.open(urls[index], '_blank').focus();
         });
     }
+
+    var article = document.querySelector("article");
+    article.addEventListener('dblclick', () => openBusinessCard());
+
+    // Handle double-tap
+    let lastTapTime = 0;
+    article.addEventListener('touchstart', (e) => {
+        const currentTime = new Date().getTime();
+        const tapInterval = currentTime - lastTapTime;
+        if (tapInterval < 300 && tapInterval > 0) {
+            e.preventDefault();
+            openBusinessCard();
+        }
+        lastTapTime = currentTime;
+    });
 }
