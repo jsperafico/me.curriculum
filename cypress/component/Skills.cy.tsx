828adbd7c6e881c5b42bb1204d136dc569b86464
Jonathan Pinto Sperafico <jhonny.potter@gmail.com>
2025-01-08 18:27:15 +0200

    skills are responsive now

--- cypress/component/Skills.cy.tsx
+++ cypress/component/Skills.cy.tsx
@@ -2,28 +2,64 @@
 
 import { Skills } from "@/components/skills"
 
-describe('Areas of expertise', () => {
-  beforeEach(() => {
-    cy.mount(<Skills />)
+import SkillsData from "../fixtures/skills.json"
+
+const THRESHOLD: number = 100;
+
+describe('Skills', () => {
+  context('General behavior', () => {
+    beforeEach(() => {
+      cy.viewport('macbook-13')
+      cy.mount(<Skills />)
+    })
+
+    SkillsData.skills.forEach((data) => {
+      it(`"${data.label}" skill is listed and have "${data.experience}/5" experience grade aside`, () => {
+        cy.get(`.list-none > :nth-child(${data.index})`).should('contain.text', data.label).should('have.class', `after:skill-${data.experience}`)
+
+        cy.get(`.list-none > :nth-child(${data.index})`).then(($li) => {
+          const rectLi = $li[0].getBoundingClientRect();
+
+          cy.get(`.list-none > :nth-child(${data.index}) > span`).then(($span) => {
+            const rectSpan = $span[0].getBoundingClientRect();
+
+            expect(rectLi.height).to.be.lessThan(rectSpan.height * 2);
+            expect(rectLi.right - rectSpan.right).to.be.lessThan(THRESHOLD);
+            expect(rectSpan.left - rectLi.left).to.be.greaterThan(THRESHOLD);
+          })
+        })
+      })
+    })
+
+    it('Skills are properly separated', () => {
+      SkillsData.spacers.forEach((data) => {
+        cy.get(`.list-none > :nth-child(${data.index})`).should('have.attr', 'data-orientation', 'horizontal')
+      })
+    })
   })
 
-  it('My skill list and level', () => {
-    cy.get('.list-none > :nth-child(1)').should('contain.text', 'Java').should('have.class', 'after:skill-5')
-    cy.get('.list-none > :nth-child(2)').should('contain.text', 'Python').should('have.class', 'after:skill-3')
-    cy.get('.list-none > :nth-child(3)').should('contain.text', 'NodeJS').should('have.class', 'after:skill-2')
-    cy.get('.list-none > :nth-child(4)').should('contain.text', 'Rust').should('have.class', 'after:skill-1')
+  context('Responsive design', () => {
+    beforeEach(() => {
+      cy.viewport("iphone-6")
+      cy.mount(<Skills />)
+    })
 
-    cy.get('.list-none > :nth-child(5)').should('have.attr', 'data-orientation', 'horizontal')
+    SkillsData.skills.forEach((data) => {
+      it(`"${data.experience}/5" experience should be bellow of "${data.label}"`, () => {
+        cy.get(`.list-none > :nth-child(${data.index})`).should('contain.text', data.label).should('have.class', `after:skill-${data.experience}`)
 
-    cy.get('.list-none > :nth-child(6)').should('contain.text', 'Selenium').should('have.class', 'after:skill-5')
-    cy.get('.list-none > :nth-child(7)').should('contain.text', 'JMeter').should('have.class', 'after:skill-4')
-    cy.get('.list-none > :nth-child(8)').should('contain.text', 'Cypress').should('have.class', 'after:skill-3')
-    cy.get('.list-none > :nth-child(9)').should('contain.text', 'RestAssured').should('have.class', 'after:skill-2')
+        cy.get(`.list-none > :nth-child(${data.index})`).then(($li) => {
+          const rectLi = $li[0].getBoundingClientRect();
 
-    cy.get('.list-none > :nth-child(5)').should('have.attr', 'data-orientation', 'horizontal')
+          cy.get(`.list-none > :nth-child(${data.index}) > span`).then(($span) => {
+            const rectSpan = $span[0].getBoundingClientRect();
 
-    cy.get('.list-none > :nth-child(11)').should('contain.text', 'Docker').should('have.class', 'after:skill-5')
-    cy.get('.list-none > :nth-child(12)').should('contain.text', 'Gitlab').should('have.class', 'after:skill-4')
-    cy.get('.list-none > :nth-child(13)').should('contain.text', 'Kubernetes').should('have.class', 'after:skill-3')
+            expect(rectLi.height).to.be.equal(rectSpan.height * 2);
+            expect(rectLi.right - rectSpan.right).to.be.lessThan(THRESHOLD);
+            expect(rectSpan.left - rectLi.left).to.be.lessThan(THRESHOLD);
+          })
+        })
+      })
+    })
   })
 })
--- src/components/skills.tsx
+++ src/components/skills.tsx
@@ -2,54 +2,52 @@ import * as React from "react"
 import { Separator } from "./ui/separator"
 import { cn } from "@/lib/utils"
 
-// TODO Warning: forwardRef render functions accept exactly two parameters: props and ref. Did you forget to use the ref parameter?
+function render(className: string | undefined, props: {}, ref: React.ForwardedRef<HTMLUListElement>) {
 
-function render(className: string | undefined, props: {}) {
-
-  return <ul className="list-none text-right m-0 start-0 content-center">
-    <li className={cn("mb-0 px-3 transition-colors group-hover:text-cyan-800 group-hover:hover:text-cyan-100 hover:bg-cyan-800 after:font-family-awesome after:skill-5", className)} {...props}>
-      <span className="pr-3">Java</span>
+  return <ul ref={ref} className="list-none text-center m-0 start-0 sm:text-right">
+    <li className={cn("mb-0 px-3 transition-colors group-hover:text-cyan-800 group-hover:hover:text-cyan-100 hover:bg-cyan-800 after:font-family-awesome after:skill-5 flex flex-col sm:block", className)} {...props}>
+      <span className="px-3">Java</span>
     </li>
-    <li className={cn("mb-0 px-3 transition-colors group-hover:text-cyan-800 group-hover:hover:text-cyan-100 hover:bg-cyan-800 after:font-family-awesome after:skill-3", className)} {...props}>
-      <span className="pr-3">Python</span>
+    <li className={cn("mb-0 px-3 transition-colors group-hover:text-cyan-800 group-hover:hover:text-cyan-100 hover:bg-cyan-800 after:font-family-awesome after:skill-3 flex flex-col sm:block", className)} {...props}>
+      <span className="px-3">Python</span>
     </li>
-    <li className={cn("mb-0 px-3 transition-colors group-hover:text-cyan-800 group-hover:hover:text-cyan-100 hover:bg-cyan-800 after:font-family-awesome after:skill-2", className)} {...props}>
-      <span className="pr-3">NodeJS</span>
+    <li className={cn("mb-0 px-3 transition-colors group-hover:text-cyan-800 group-hover:hover:text-cyan-100 hover:bg-cyan-800 after:font-family-awesome after:skill-2 flex flex-col sm:block", className)} {...props}>
+      <span className="px-3">NodeJS</span>
     </li>
-    <li className={cn("mb-0 px-3 transition-colors group-hover:text-cyan-800 group-hover:hover:text-cyan-100 hover:bg-cyan-800 after:font-family-awesome after:skill-1", className)} {...props}>
-      <span className="pr-3">Rust</span>
+    <li className={cn("mb-0 px-3 transition-colors group-hover:text-cyan-800 group-hover:hover:text-cyan-100 hover:bg-cyan-800 after:font-family-awesome after:skill-1 flex flex-col sm:block", className)} {...props}>
+      <span className="px-3">Rust</span>
     </li>
     <Separator className="my-1 border-cyan-800 border-y" orientation="horizontal"></Separator>
-    <li className={cn("mb-0 px-3 transition-colors group-hover:text-cyan-800 group-hover:hover:text-cyan-100 hover:bg-cyan-800 after:font-family-awesome after:skill-5", className)} {...props}>
-      <span className="pr-3">Selenium</span>
+    <li className={cn("mb-0 px-3 transition-colors group-hover:text-cyan-800 group-hover:hover:text-cyan-100 hover:bg-cyan-800 after:font-family-awesome after:skill-5 flex flex-col sm:block", className)} {...props}>
+      <span className="px-3">Selenium</span>
     </li>
-    <li className={cn("mb-0 px-3 transition-colors group-hover:text-cyan-800 group-hover:hover:text-cyan-100 hover:bg-cyan-800 after:font-family-awesome after:skill-4", className)} {...props}>
-      <span className="pr-3">JMeter</span>
+    <li className={cn("mb-0 px-3 transition-colors group-hover:text-cyan-800 group-hover:hover:text-cyan-100 hover:bg-cyan-800 after:font-family-awesome after:skill-4 flex flex-col sm:block", className)} {...props}>
+      <span className="px-3">JMeter</span>
     </li>
-    <li className={cn("mb-0 px-3 transition-colors group-hover:text-cyan-800 group-hover:hover:text-cyan-100 hover:bg-cyan-800 after:font-family-awesome after:skill-3", className)} {...props}>
-      <span className="pr-3">Cypress</span>
+    <li className={cn("mb-0 px-3 transition-colors group-hover:text-cyan-800 group-hover:hover:text-cyan-100 hover:bg-cyan-800 after:font-family-awesome after:skill-3 flex flex-col sm:block", className)} {...props}>
+      <span className="px-3">Cypress</span>
     </li>
-    <li className={cn("mb-0 px-3 transition-colors group-hover:text-cyan-800 group-hover:hover:text-cyan-100 hover:bg-cyan-800 after:font-family-awesome after:skill-2", className)} {...props}>
-      <span className="pr-3">RestAssured</span>
+    <li className={cn("mb-0 px-3 transition-colors group-hover:text-cyan-800 group-hover:hover:text-cyan-100 hover:bg-cyan-800 after:font-family-awesome after:skill-2 flex flex-col sm:block", className)} {...props}>
+      <span className="px-3">RestAssured</span>
     </li>
     <Separator className="my-1 border-cyan-800 border-y" orientation="horizontal"></Separator>
-    <li className={cn("mb-0 px-3 transition-colors group-hover:text-cyan-800 group-hover:hover:text-cyan-100 hover:bg-cyan-800 after:font-family-awesome after:skill-5", className)} {...props}>
-      <span className="pr-3">Docker</span>
+    <li className={cn("mb-0 px-3 transition-colors group-hover:text-cyan-800 group-hover:hover:text-cyan-100 hover:bg-cyan-800 after:font-family-awesome after:skill-5 flex flex-col sm:block", className)} {...props}>
+      <span className="px-3">Docker</span>
     </li>
-    <li className={cn("mb-0 px-3 transition-colors group-hover:text-cyan-800 group-hover:hover:text-cyan-100 hover:bg-cyan-800 after:font-family-awesome after:skill-4", className)} {...props}>
-      <span className="pr-3">Gitlab</span>
+    <li className={cn("mb-0 px-3 transition-colors group-hover:text-cyan-800 group-hover:hover:text-cyan-100 hover:bg-cyan-800 after:font-family-awesome after:skill-4 flex flex-col sm:block", className)} {...props}>
+      <span className="px-3">Gitlab</span>
     </li>
-    <li className={cn("mb-0 px-3 transition-colors group-hover:text-cyan-800 group-hover:hover:text-cyan-100 hover:bg-cyan-800 after:font-family-awesome after:skill-3", className)} {...props}>
-      <span className="pr-3">Kubernetes</span>
+    <li className={cn("mb-0 px-3 transition-colors group-hover:text-cyan-800 group-hover:hover:text-cyan-100 hover:bg-cyan-800 after:font-family-awesome after:skill-3 flex flex-col sm:block", className)} {...props}>
+      <span className="px-3">Kubernetes</span>
     </li>
   </ul>
 }
 
 const Skills = React.forwardRef<
-  HTMLDivElement,
-  React.HTMLAttributes<HTMLDivElement>
->(({ className, ...props }) => (
-  render(className, props)
+  HTMLUListElement,
+  React.HTMLAttributes<HTMLUListElement>
+>(({ className, ...props }, ref) => (
+  render(className, props, ref)
 ))
 
 Skills.displayName = "Skills"
