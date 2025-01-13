8f837103186568d1d2dad02f5e4de1533b4bbddb
Jonathan Pinto Sperafico <jhonny.potter@gmail.com>
2025-01-13 23:07:40 +0200

    business card responsive

--- cypress/page/BusinessCard.cy.tsx
+++ cypress/page/BusinessCard.cy.tsx
@@ -50,20 +50,21 @@ describe('Business Card page', () => {
       cy.get('[data-id="front"] h1').should('have.text', 'Jonathan Pinto Sperafico')
       cy.get('[data-id="front"] h2').should('have.text', 'Sr. Test Automation Engineer')
 
+      cy.get('[data-id="front"]').realHover()
+
+      cy.get('[data-id="back"] [data-id="skills"] ul').should('be.visible')
+      cy.get('[data-id="back"] [data-id="contacts"] ul li').should('be.visible')
+    })
+
+    it('Should be nicely resized fit in "Landscape"', () => {
+      cy.viewport('iphone-6', 'landscape')
+
+      cy.get('[data-id="front"] .aspect-square').should('have.attr', 'src').and('contain', 'src/assets/me.jpg')
+      cy.get('[data-id="front"] h1').should('have.text', 'Jonathan Pinto Sperafico')
+      cy.get('[data-id="front"] h2').should('have.text', 'Sr. Test Automation Engineer')
+
       cy.get('[data-id="back"] [data-id="skills"] ul').find('li').should('have.length', 11)
       cy.get('[data-id="back"] [data-id="contacts"] ul').find('li').should('have.length', 4)
     })
-    /* 
-        it('Should be nicely resized fit in "Landscape"', () => {
-          cy.viewport('iphone-6', 'landscape')
-    
-          cy.get('[data-id="front"] .aspect-square').should('have.attr', 'src').and('contain', 'src/assets/me.jpg')
-          cy.get('[data-id="front"] h1').should('have.text', 'Jonathan Pinto Sperafico')
-          cy.get('[data-id="front"] h2').should('have.text', 'Sr. Test Automation Engineer')
-    
-          cy.get('[data-id="back"] [data-id="skills"] ul').find('li').should('have.length', 11)
-          cy.get('[data-id="back"] [data-id="contacts"] ul').find('li').should('have.length', 4)
-        })
-    */
   })
 })
--- src/pages/business-card.tsx
+++ src/pages/business-card.tsx
@@ -18,23 +18,23 @@ const BusinessCardPage = React.forwardRef<
     ref={ref}
     className={cn("max-w-[90mm] max-h-[50mm] sm:max-w-[135mm] sm:max-h-[75mm] md:max-w-[180mm] md:max-h-[100mm] perspective-1000 group fixed m-auto inset-0", className)}
     {...props}>
-    <Card data-id="front" className="w-[90mm] h-[50mm] sm:w-[135mm] sm:h-[75mm] md:w-[180mm] md:h-[100mm] backface-hidden transform transition duration-[1.5s] group-hover:rotate-y-180 absolute grid grid-flow-col auto-cols-max rounded-xl border-2 bg-cyan-100 text-cyan-800 shadow-cyan-700 border-cyan-100 shadow-lg">
-      <CardHeader className="max-w-[288px] m-2 rounded-xl bg-cyan-800 text-cyan-100 shadow-cyan-800 shadow-md">
-        <Avatar className="hex m-auto size-60 rounded-none">
+    <Card data-id="front" className="w-[90mm] h-[50mm] sm:w-[135mm] sm:h-[75mm] md:w-[180mm] md:h-[100mm] backface-hidden transform transition duration-[1.5s] group-hover:rotate-y-180 absolute grid grid-flow-col grid-cols-5 rounded-xl border-2 bg-cyan-100 text-cyan-800 shadow-cyan-700 border-cyan-100 shadow-lg">
+      <CardHeader className="max-w-[288px] m-2 p-0 rounded-xl bg-cyan-800 text-cyan-100 shadow-cyan-800 shadow-md col-span-2">
+        <Avatar className="hex m-auto size-24 sm:size-40 md:size-52 rounded-none">
           <AvatarImage src={picMe} />
         </Avatar>
       </CardHeader>
-      <CardContent className="m-auto w-auto prose prose-sm">
+      <CardContent className="m-auto w-auto col-span-3 prose prose-sm p-1 md:p-6 md:pt-0">
         <h1 className="text-center text-cyan-800 italic font-bold mb-0">Jonathan Pinto Sperafico</h1>
         <h2 className="text-center text-cyan-800 italic font-bold mt-3">Sr. Test Automation Engineer</h2>
       </CardContent>
     </Card>
 
-    <Card data-id="back" className="w-[90mm] h-[50mm] sm:w-[135mm] sm:h-[75mm] md:w-[180mm] md:h-[100mm] backface-hidden transform transition duration-[1.5s] -rotate-y-180 group-hover:rotate-y-0 absolute insert-center grid grid-flow-col auto-cols-fr rounded-xl border-2 bg-cyan-100 text-cyan-800 shadow-cyan-700 border-cyan-100 shadow-lg">
-      <CardHeader data-id="skills" className="m-auto w-full prose prose-sm">
-        <Skills className="text-cyan-800" />
+    <Card data-id="back" className="w-[90mm] h-[50mm] sm:w-[135mm] sm:h-[75mm] md:w-[180mm] md:h-[100mm] backface-hidden transform transition duration-[1.5s] -rotate-y-180 group-hover:rotate-y-0 absolute insert-center grid grid-flow-col grid-cols-3 rounded-xl border-2 bg-cyan-100 text-cyan-800 shadow-cyan-700 border-cyan-100 shadow-lg">
+      <CardHeader data-id="skills" className="m-auto w-full prose prose-sm p-0 md:p-6 col-span-3">
+        <Skills className="text-cyan-800 pr-5" />
       </CardHeader>
-      <CardContent data-id="contacts" className="max-w-[288px] justify-self-end m-2 rounded-xl bg-cyan-800 text-cyan-100 shadow-cyan-800 shadow-md pt-4 content-center">
+      <CardContent data-id="contacts" className="max-w-[288px] justify-self-end m-2 px-10 py-0 rounded-xl bg-cyan-800 text-cyan-100 shadow-cyan-800 shadow-md content-center col-span-2 sm:px-5">
         <Social />
       </CardContent>
     </Card>
