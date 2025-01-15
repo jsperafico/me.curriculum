63871b8f4f2e7d6754d3f0a6598d2a81dd877ea5
Jonathan Pinto Sperafico <jhonny.potter@gmail.com>
2025-01-15 16:57:58 +0200

    business card font size fixed

--- src/pages/business-card.tsx
+++ src/pages/business-card.tsx
@@ -25,17 +25,17 @@ const BusinessCardPage = React.forwardRef<
         </Avatar>
       </CardHeader>
       <CardContent className="m-auto w-auto col-span-3 p-1 md:p-6 md:pt-0">
-        <h1 className="text-center text-cyan-800 italic font-bold mb-0">Jonathan Pinto Sperafico</h1>
-        <h2 className="text-center text-cyan-800 italic font-bold mt-3">Sr. Test Automation Engineer</h2>
+        <h1 className="text-2xl text-center text-cyan-800 italic font-bold mb-0 md:text-3xl">Jonathan Pinto Sperafico</h1>
+        <h2 className="text-lg text-center text-cyan-800 italic font-bold mt-3 md:text-xl">Sr. Test Automation Engineer</h2>
       </CardContent>
     </Card>
 
     <Card data-id="back" className="w-[90mm] h-[50mm] sm:w-[135mm] sm:h-[75mm] md:w-[180mm] md:h-[100mm] backface-hidden transform transition duration-[1.5s] -rotate-y-180 group-hover:rotate-y-0 absolute insert-center grid grid-flow-col grid-cols-3 rounded-xl border-2 bg-cyan-100 text-cyan-800 shadow-cyan-700 border-cyan-100 shadow-lg">
       <CardHeader data-id="skills" className="m-auto w-full p-0 md:p-6 col-span-3">
-        <Skills className="text-cyan-800 p-0" />
+        <Skills className="text-cyan-800 p-0 text-sm md:text-lg" />
       </CardHeader>
-      <CardContent data-id="contacts" className="max-w-[288px] justify-self-end m-2 px-10 py-0 rounded-xl bg-cyan-800 text-cyan-100 shadow-cyan-800 shadow-md content-center col-span-2 sm:px-5">
-        <Social />
+      <CardContent data-id="contacts" className="max-w-[288px] justify-self-end m-2 px-10 py-0 rounded-xl bg-cyan-800 text-cyan-100 shadow-cyan-800 shadow-md content-center col-span-2 sm:px-5 md:px-2">
+        <Social className="text-sm md:text-base" />
       </CardContent>
     </Card>
   </article >
