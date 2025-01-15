3f3d549ab25d2ed16dbbd12dbdf6aa0ec7a04104
Jonathan Pinto Sperafico <jhonny.potter@gmail.com>
2025-01-15 21:31:36 +0200

    social redesign and business card adjusted

--- src/components/social.tsx
+++ src/components/social.tsx
@@ -4,32 +4,32 @@ import { cn } from "@/lib/utils"
 
 function render(className: string | undefined, props: {}, ref: React.ForwardedRef<HTMLUListElement>) {
 
-  return <ul ref={ref} className="list-none text-left p-0 mx-3">
+  return <ul ref={ref} className="list-none text-left p-0 m-0">
     <li className={cn("my-3 px-3 cursor-pointer", className)} {...props}>
       <a className="w-full" href="https://www.jsperafico.me" target="_blank">
         <div className="flex flex-row before:font-family-awesome before:social-website">
-          <span className="hidden md:block before:mr-5">https://www.jsperafico.me</span>
+          <span className="portrait:hidden before:mr-5">https://www.jsperafico.me</span>
         </div>
       </a>
     </li>
     <li className={cn("my-3 px-3 cursor-pointer", className)} {...props}>
       <a className="w-full" href="https://www.linkedin.com/in/jonathanpintosperafico/" target="_blank">
         <div className="flex flex-row before:font-family-awesome before:social-linkedin">
-          <span className="hidden md:block before:mr-5">jonathanpintosperafico</span>
+          <span className="portrait:hidden before:mr-5">jonathanpintosperafico</span>
         </div>
       </a>
     </li>
     <li className={cn("my-3 px-3 cursor-pointer", className)} {...props}>
       <a className="w-full" href="https://github.com/jsperafico" target="_blank">
         <div className="flex flex-row before:font-family-awesome before:social-github">
-          <span className="hidden md:block before:mr-5">jsperafico</span>
+          <span className="portrait:hidden before:mr-5">jsperafico</span>
         </div>
       </a>
     </li>
     <li className={cn("my-3 px-3 cursor-pointer", className)} {...props}>
       <a className="w-full" href="mailto:hire@jsperafico.me">
         <div className="flex flex-row before:font-family-awesome before:social-mail">
-          <span className="hidden md:block before:mr-5">hire@jsperafico.me</span>
+          <span className="portrait:hidden before:mr-5">hire@jsperafico.me</span>
         </div>
       </a>
     </li>
--- src/pages/business-card.tsx
+++ src/pages/business-card.tsx
@@ -16,9 +16,9 @@ const BusinessCardPage = React.forwardRef<
 
   <article
     ref={ref}
-    className={cn("max-w-[90mm] max-h-[50mm] md:max-w-[180mm] md:max-h-[100mm] perspective-1000 group fixed m-auto xs:mt-auto md:mt-auto inset-0", className)}
+    className={cn("portrait:max-w-[90mm] sm:max-w-[120mm] max-h-[50mm] md:max-w-[180mm] md:max-h-[100mm] perspective-1000 group fixed m-auto xs:mt-auto md:mt-auto inset-0", className)}
     {...props}>
-    <Card data-id="front" className="w-[90mm] h-[50mm] md:w-[180mm] md:h-[100mm] backface-hidden transform transition duration-[1.5s] group-hover:rotate-y-180 absolute grid grid-flow-col grid-cols-5 rounded-xl border-2 bg-cyan-100 text-cyan-800 shadow-cyan-700 border-cyan-100 shadow-lg">
+    <Card data-id="front" className="portrait:w-[90mm] sm:w-[120mm] h-[50mm] md:w-[180mm] md:h-[100mm] backface-hidden transform transition duration-[1.5s] group-hover:rotate-y-180 absolute grid grid-flow-col grid-cols-5 rounded-xl border-2 bg-cyan-100 text-cyan-800 shadow-cyan-700 border-cyan-100 shadow-lg">
       <CardHeader className="max-w-[288px] m-2 p-0 rounded-xl bg-cyan-800 text-cyan-100 shadow-cyan-800 shadow-md col-span-2">
         <Avatar className="hex m-auto size-24 md:size-52 rounded-none">
           <AvatarImage src={picMe} />
@@ -30,11 +30,11 @@ const BusinessCardPage = React.forwardRef<
       </CardContent>
     </Card>
 
-    <Card data-id="back" className="w-[90mm] h-[50mm] md:w-[180mm] md:h-[100mm] backface-hidden transform transition duration-[1.5s] -rotate-y-180 group-hover:rotate-y-0 absolute insert-center grid grid-flow-col grid-cols-3 rounded-xl border-2 bg-cyan-100 text-cyan-800 shadow-cyan-700 border-cyan-100 shadow-lg">
+    <Card data-id="back" className="portrait:w-[90mm] sm:w-[120mm] h-[50mm] md:w-[180mm] md:h-[100mm] backface-hidden transform transition duration-[1.5s] -rotate-y-180 group-hover:rotate-y-0 absolute insert-center grid grid-flow-col grid-cols-3 rounded-xl border-2 bg-cyan-100 text-cyan-800 shadow-cyan-700 border-cyan-100 shadow-lg">
       <CardHeader data-id="skills" className="m-auto w-full p-0 md:p-6 col-span-3">
         <Skills className="text-cyan-800 p-0 text-sm md:text-lg" />
       </CardHeader>
-      <CardContent data-id="contacts" className="max-w-[288px] justify-self-end m-2 px-10 py-0 rounded-xl bg-cyan-800 text-cyan-100 shadow-cyan-800 shadow-md content-center col-span-2 md:px-2">
+      <CardContent data-id="contacts" className="max-w-[288px] justify-self-end m-2 p-0 rounded-xl bg-cyan-800 text-cyan-100 shadow-cyan-800 shadow-md content-center col-span-2 portrait:px-4 md:px-2">
         <Social className="text-sm md:text-base" />
       </CardContent>
     </Card>
