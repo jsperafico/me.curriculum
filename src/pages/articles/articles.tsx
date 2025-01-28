40b19c8c8d3dd09325eca945fe5b20bd5c652ea8
Jonathan Pinto Sperafico <jhonny.potter@gmail.com>
2025-01-28 19:08:08 +0200

    articles list wth sufficient layout and information

--- src/pages/articles/articles.tsx
+++ src/pages/articles/articles.tsx
@@ -1,4 +1,4 @@
-import { Card, CardContent, CardHeader } from "../../components/ui/card"
+import { Card, CardContent, CardFooter, CardHeader } from "../../components/ui/card"
 
 import { zodResolver } from "@hookform/resolvers/zod"
 import { useForm } from "react-hook-form"
@@ -16,24 +16,21 @@ import {
 import { Input } from "@/components/ui/input"
 import { Separator } from "@/components/ui/separator"
 
-const articles = [
-  {
-    id: '17823yeoqwiuhihe708daw08r0j1',
-    title: 'This is my super article',
-    summary: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum"
-  },
-]
+import ArticlesFile from "@/articles.json"
 
 export const ArticlesPage = () => {
 
-  console.log(encodeURI(import.meta.env.VITE_SECRET_ARTICLE_EDITOR))
+  if (import.meta.env.DEV) {
+    console.log(encodeURI(import.meta.env.VITE_SECRET_ARTICLE_EDITOR))
+  }
 
-  const articlesList = articles.map(article => {
+  const articlesList = ArticlesFile.articles.map(article => {
     return (
-      <a href={'/articles/' + article.id}>
+      <a key={article.id} href={'/articles/' + article.id}>
         <Card className="max-h-56 p-4 bg-cyan-800 text-cyan-100 shadow-cyan-700 border-cyan-100 shadow-lg transition-colors delay-150 duration-500 hover:bg-cyan-100 hover:text-cyan-800 hover:border-cyan-800">
           <CardHeader className="my-2 mx-6 p-0">{article.title}</CardHeader>
           <CardContent className="line-clamp-1  text-ellipsis">{article.summary}</CardContent>
+          <CardFooter className="mx-6 p-0">{new Date(article.published).toLocaleString()}</CardFooter>
         </Card>
       </a>
     )
@@ -79,7 +76,7 @@ export const ArticlesPage = () => {
         </Form>
       </section>
       <Separator className="m-auto w-[80%] border-y border-cyan-800" orientation="horizontal"></Separator>
-      <section className={"h-full w-full grid grid-cols-1 gap-4 p-4 ".concat((articles.length > 1) ? "md:grid-cols-2" : "")}>
+      <section className={"h-full w-full grid grid-cols-1 gap-4 p-4 ".concat((articlesList.length > 1) ? "md:grid-cols-2" : "")}>
         {articlesList}
       </section>
     </article>
